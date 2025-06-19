629a8365a83254ae3ab3f41851cf50e30f807fa5
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-06-19 17:29:42 +0530

    Second Commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const age = 24;
 const name = "Ayush";
 const dob = "20/11/2004";
 
-function sayName(){
+function sayName(name){
     console.log("My Name is " + name);
+}
+function sayAge(age){
+    console.log("My Age is ", age);
 }
